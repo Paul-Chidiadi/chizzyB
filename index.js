93cfa097ab95ebdfe3ee3a24f27f22966e65ffe6
Paul Chidiadi <paulchidiadi@gmail.com>
2022-07-10 20:54:35 +0100

    added index.js, created pop ups, added javascript code for toggling pop-ups

---
+++ index.js
@@ -0,0 +1,25 @@
+const join = document.querySelector("#join"); // Pop up for academy
+const ready = document.querySelector("#readyy"); // Pop up for ready store
+const botn = document.querySelector(".cancel");
+const bottn = document.querySelector("#cancell");
+const overlay = document.querySelector("#overlay");
+
+botn.addEventListener("click", () => {
+  document.querySelector("#joinpop").classList.remove("active");
+  document.querySelector("#readypop").classList.remove("active");
+  overlay.classList.remove("active");
+});
+bottn.addEventListener("click", () => {
+  document.querySelector("#joinpop").classList.remove("active");
+  overlay.classList.remove("active");
+});
+join.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("#joinpop").classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+readyy.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("#readypop").classList.toggle("active");
+  overlay.classList.toggle("active");
+});
