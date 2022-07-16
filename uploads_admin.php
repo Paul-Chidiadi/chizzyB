401bf4e68b28e4f12efbdf5d6648944e69230af3
Paul Chidiadi <paulchidiadi@gmail.com>
2022-07-17 00:51:37 +0100

    created upload section

--- uploads_admin.php
+++ uploads_admin.php
@@ -16,41 +16,56 @@
   </head>
   <body>
 
-    <div class="body">
+    <div class="bodyy">
+        <div class="body">
 
-        <!-- NAVIGATION BAR -->
-        <div class="header">
-            <div class="logo">CHIZZYB COUTURE</div>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="index.html">HOME</a>
-                        <div class="indicator"></div>
-                    </li>
-                    <li>
-                        <a href="">UPLOAD</a>
-                        <div class="indicator"></div>
-                    </li>
-                    <li>
-                        <a href="">ACADEMY</a>
-                        <div class="indicator"></div>
-                    </li>
-                    <li>
-                        <a href="">BE SPOKE ORDERS</a>
-                        <div class="indicator"></div>
-                    </li>
-                    <li>
-                        <a href="">READY ORDERS</a>
-                        <div class="indicator"></div>
-                    </li>
-                </ul>
-            </nav>
+            <!-- NAVIGATION BAR -->
+            <div class="header">
+                <div class="logo">CHIZZYB COUTURE</div>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="index.html">HOME</a>
+                            <div class="indicator"></div>
+                        </li>
+                        <li>
+                            <a href="#upload">UPLOAD</a>
+                            <div class="indicator"></div>
+                        </li>
+                        <li>
+                            <a href="">ACADEMY</a>
+                            <div class="indicator"></div>
+                        </li>
+                        <li>
+                            <a href="">BE SPOKE ORDERS</a>
+                            <div class="indicator"></div>
+                        </li>
+                        <li>
+                            <a href="">READY ORDERS</a>
+                            <div class="indicator"></div>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+            <!-- UPLOAD -->
+            <div id="upload" class="upload">
+                <div class="title">UPLOAD</div>
+                <form action="uploads_admin.php" class="product" method="post" enctype="multipart/form-data">
+                    <label>Input info for the product you want to add</label>
+                    <input type="text" name="prodname" class="control" placeholder="product name" required>
+                    <input type="text" name="prodprice" class="control" placeholder="product price eg. $50" required>
+                    <textarea type="text" name="proddetails" class="control" placeholder="product detail" required>
+                    </textarea>
+                    <label>Input image of the product here</label>
+                    <input type="file" name="file" class="control" required> 
+                    <button  type="submit" name="submit" class="btn">UPLOAD</button>
+                </form>
+            </div>
+
         </div>
 
-        <!-- UPLOAD -->
-        <div class="upload">
-            <div class="title">UPLOAD</div>
-        </div>
+        
         
     </div>
 
