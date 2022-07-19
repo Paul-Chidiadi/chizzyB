81afa9fd94f7afa19fade75b44329db9c3772228
Paul Chidiadi <paulchidiadi@gmail.com>
2022-07-19 08:52:11 +0100

    edited index.html and index.js, removed popup when ready store is clicked, added link that takes user to login page to access store when they click on ready to wear

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 // code for POP-UPS inside index.html
 const join = document.querySelector("#join"); // Pop up for academy
-const ready = document.querySelector("#readyy"); // Pop up for ready store
-const botn = document.querySelector(".cancel");
+
 const bottn = document.querySelector("#cancell");
 const overlay = document.querySelector("#overlay");
 
-botn.addEventListener("click", () => {
-  document.querySelector("#joinpop").classList.remove("active");
-  document.querySelector("#readypop").classList.remove("active");
-  overlay.classList.remove("active");
-});
 bottn.addEventListener("click", () => {
   document.querySelector("#joinpop").classList.remove("active");
   overlay.classList.remove("active");
@@ -17,10 +11,5 @@ bottn.addEventListener("click", () => {
 join.addEventListener("click", (e) => {
   e.preventDefault();
   document.querySelector("#joinpop").classList.toggle("active");
-  overlay.classList.toggle("active");
-});
-ready.addEventListener("click", (e) => {
-  e.preventDefault();
-  document.querySelector("#readypop").classList.toggle("active");
   overlay.classList.toggle("active");
 });
