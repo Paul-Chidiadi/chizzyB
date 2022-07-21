e8614d44e9ad4f8308c2655d6b2a8781c1c7ea33
Paul Chidiadi <paulchidiadi@gmail.com>
2022-07-21 18:33:37 +0100

    added sell.css for styling, created navbar in sell.php

--- sell.php
+++ sell.php
@@ -17,15 +17,59 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
     <meta content="" name="keywords" />
     <meta content="" name="description" />
-    <title>CHIZZYB | SELL | PRODUCTS</title>
+    <title>CHIZZYB | STORE | PRODUCTS</title>
 
     <!--MAin CSS file-->
-    <link rel="stylesheet" href="css/access.css" />
+    <link rel="stylesheet" href="css/sell.css" />
     <!--BOXICONS CSS-->
     <link
       href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css"
       rel="stylesheet"
     />
   </head>
-  <body></body>
+  <body>
+
+    <!-- NAVIGATION BAR -->
+    <div class="header">
+      <div class="logo">CHIZZYB COUTURE</div>
+      <nav>
+          <ul>
+            <li class="list">
+              <a href="">
+                  <span class="icon"><i class='bx bx-home'></i></span>
+                  <span class="text">HOME</span>
+              </a>  
+            </li>
+            <li class="list">
+                <a href="">
+                  <span class="icon"><i class='bx bx-store' ></i></span>
+                  <span class="text">PRODUCTS</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="">
+                  <span class="icon"><i class='bx bx-message-alt-dots'></i></span>
+                  <span class="text">CONTACTS</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="">
+                  <span class="icon"><i class='bx bx-log-out-circle'></i></span>
+                  <span class="text">LOGOUT</span>
+                </a>
+            </li>
+            <div class="indicator"></div>
+          </ul>
+      </nav>
+      <!-- menu button -->
+      <div class="menubtn">
+          <i class="bx bx-menu"></i>
+      </div>
+    </div>
+
+    
+
+
+
+  </body>
 </html>
